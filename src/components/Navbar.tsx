7db2d6a6d166cd819f6a29761c40c855dd0320ad
So Momma <sol0608sol@gmail.com>
2024-11-22 15:42:35 +0800

    Add @radix-ui/react-slot dependency, update primary and ring colors in globals.css, and create Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div className="bg-white sticky z-50 top-0 insert-x-0 h-16">
+            <header className="relative "></header>
+        </div>
+    )
+}
+
+export default Navbar
